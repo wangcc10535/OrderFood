8682070e4931b163767709323a8d35416466139b
wangcc <1053578651@qq.com>
2023-01-06 00:17:59 +0800

    项目初始化

---
+++ src/views/datareportsIMgr/ReportMgrIndex.vue
@@ -0,0 +1,30 @@
+<!--
+ * @Author: wangcc 1053578651@qq.com
+ * @Date: 2023-01-05 22:49:42
+ * @LastEditors: wangcc 1053578651@qq.com
+ * @LastEditTime: 2023-01-05 23:04:33
+ * @FilePath: \orderfood\src\views\datareportsIMgr\reportMgrIndex.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
+<template>
+    <div>
+        统计报表
+    </div>
+</template>
+<script>
+export default {
+    name:'reportMgrIndex',
+    data() {
+        return {
+        }
+    },
+    created() {
+    },
+    mounted() {
+    },
+    methods: {
+    }
+};
+</script>
+<style scoped lang='scss'>
+</style>
---
+++ src/views/memberIMgr/memberMgrIndex.vue
@@ -0,0 +1,30 @@
+<!--
+ * @Author: wangcc 1053578651@qq.com
+ * @Date: 2023-01-05 22:36:26
+ * @LastEditors: wangcc 1053578651@qq.com
+ * @LastEditTime: 2023-01-05 22:36:37
+ * @FilePath: \orderfood\src\views\memberIMgr\memberMgrIndex.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
+<template>
+    <div>
+        会员管理
+    </div>
+</template>
+<script>
+export default {
+    name:'memberMgrIndex',
+    data() {
+        return {
+        }
+    },
+    created() {
+    },
+    mounted() {
+    },
+    methods: {
+    }
+};
+</script>
+<style scoped lang='scss'>
+</style>
