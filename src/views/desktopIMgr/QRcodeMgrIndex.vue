e7dbf49f49d211f13e04d305c9a62fbceea24b61
wangcc <1053578651@qq.com>
2023-01-06 13:45:31 +0800

    管理页面添加

---
+++ src/views/desktopIMgr/QRcodeMgrIndex.vue
@@ -0,0 +1,30 @@
+<!--
+ * @Author: wangcc 1053578651@qq.com
+ * @Date: 2023-01-06 13:37:00
+ * @LastEditors: wangcc 1053578651@qq.com
+ * @LastEditTime: 2023-01-06 13:37:13
+ * @FilePath: \orderfood\src\views\desktopIMgr\QRcodeMgrIndex.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
+<template>
+    <div>
+        二维码管理
+    </div>
+</template>
+<script>
+export default {
+    name:'QRcodeMgrIndex',
+    data() {
+        return {
+        }
+    },
+    created() {
+    },
+    mounted() {
+    },
+    methods: {
+    }
+};
+</script>
+<style scoped lang='scss'>
+</style>
---
+++ src/views/desktopIMgr/desktopMgrIndex.vue
@@ -0,0 +1,30 @@
+<!--
+ * @Author: wangcc 1053578651@qq.com
+ * @Date: 2023-01-06 13:36:07
+ * @LastEditors: wangcc 1053578651@qq.com
+ * @LastEditTime: 2023-01-06 13:43:05
+ * @FilePath: \orderfood\src\views\desktopIMgr\desktopMgrIndex.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AEzhuom
+-->
+<template>
+    <div>
+        打印机管理
+    </div>
+</template>
+<script>
+export default {
+    name:'desktopMgrIndex',
+    data() {
+        return {
+        }
+    },
+    created() {
+    },
+    mounted() {
+    },
+    methods: {
+    }
+};
+</script>
+<style scoped lang='scss'>
+</style>
