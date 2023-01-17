b72d4950223b95492d1e7ffeb99991facf3b20b9
wangcc <1053578651@qq.com>
2023-01-17 08:36:15 +0800

    系统新增

--- src/views/dishesIMgr/dishesMgrIndex.vue
+++ src/views/dishesIMgr/dishesMgrIndex.vue
@@ -2,7 +2,7 @@
  * @Author: wangcc 1053578651@qq.com
  * @Date: 2023-01-05 22:20:04
  * @LastEditors: wangcc 1053578651@qq.com
- * @LastEditTime: 2023-01-15 16:53:39
+ * @LastEditTime: 2023-01-17 04:25:45
  * @FilePath: \orderfood\src\views\dishesIMgr\dishesMgrIndex.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
